70a249e824f6985c22e55c2884eeca4f624d45c5
Daniel <meskal1@rambler.ru>
2022-02-07 19:24:40 +0300

    задачa_12

--- script.js
+++ script.js
@@ -57,6 +57,9 @@ const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
 
 // Only change code below this line
-const increment = (number, value=1) => number + value;
+const increment = (number, value = 1) => number + value;
 // Only change code above this line
 
+const sum = (...args) => {
+	return args.reduce((a, b) => a + b, 0);
+}
