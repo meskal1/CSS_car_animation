f63f892065dec9b1c77f7abf3626dc1473a4a698
Daniel <meskal1@rambler.ru>
2022-02-07 17:11:12 +0300

    задачa_7

--- script.js
+++ script.js
@@ -16,9 +16,21 @@ myVar--;
 function checkScope() {
 	let i = 'function scope';
 	if (true) {
-	 let i = 'block scope';
-	  console.log('Block scope i is: ', i);
+		let i = 'block scope';
+		console.log('Block scope i is: ', i);
 	}
 	console.log('Function scope i is: ', i);
 	return i;
- }
+}
+
+const s = [5, 7, 2];
+function editInPlace() {
+	// Only change code below this line
+	// return s.sort((a,b)=> a-b);
+	s[0] = 2;
+	s[1] = 5;
+	s[2] = 7;
+	// Using s = [2, 5, 7] would be invalid
+	// Only change code above this line
+}
+editInPlace();
