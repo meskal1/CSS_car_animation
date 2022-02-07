db6e3d722fcf26bbee12d22aff934db70d0e92fc
Daniel <meskal1test@gmail.com>
2022-02-07 12:09:23 +0300

    задачи_2-6

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 'use strict';
 
 const sum = 10 + 10;
+const difference = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
+
+let myVar = 87;
+// Змініть код лише під цим рядком
+myVar++;
+
+let myVar = 87;
+// Змініть код лише під цим рядком
+myVar--;
+
+function checkScope() {
+	let i = 'function scope';
+	if (true) {
+	 let i = 'block scope';
+	  console.log('Block scope i is: ', i);
+	}
+	console.log('Function scope i is: ', i);
+	return i;
+ }
