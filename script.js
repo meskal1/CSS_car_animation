5657b3a954a2d73010804d85c69b483050e70852
Daniel <meskal1@rambler.ru>
2022-02-08 16:09:47 +0300

    задачa_23

--- script.js
+++ script.js
@@ -152,3 +152,22 @@ const bicycle = {
 // Only change code above this line
 bicycle.setGear(3);
 console.log(bicycle.gear);
+
+// Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+	const teaCups = [];
+	for (let cups = 1; cups <= numOfCups; cups += 1) {
+		const teaCup = prepareTea();
+		teaCups.push(teaCup);
+	}
+	return teaCups;
+};
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+// Only change code above this line
