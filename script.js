dc71daf7e641cbbcc1cc35a43278f472b1211ded
Daniel <meskal1@rambler.ru>
2022-02-08 19:30:17 +0300

    задачa_39

--- script.js
+++ script.js
@@ -233,3 +233,16 @@ const myStr = "My name is " + myName + " and I am well!";
 const someAdjective = "123";
 let myStr = "Learning to code is ";
 myStr += someAdjective;
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
