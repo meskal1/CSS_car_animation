0bf16e7afdfafdd42184c3d5e3cda23c33ef852d
Daniel <meskal1@rambler.ru>
2022-02-08 17:46:26 +0300

    задачa_33

--- script.js
+++ script.js
@@ -215,3 +215,5 @@ b /= 4;
 c /= 11;
 
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
