d06bfdccd11a4f7f1706ac4629898b15152c8259
Daniel <meskal1@rambler.ru>
2022-02-08 21:21:06 +0300

    задачa_55

--- script.js
+++ script.js
@@ -310,3 +310,8 @@ myArray.push(["dog", 3]);
 const myArray = [["John", 23], ["cat", 2]];
 // Only change code below this line
 const removedFromMyArray = myArray.pop();
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
