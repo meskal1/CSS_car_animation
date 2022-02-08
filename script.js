9f89c431a5b1051ca4f7734e43065fb768a02091
Daniel <meskal1@rambler.ru>
2022-02-08 21:25:24 +0300

    задачa_57

--- script.js
+++ script.js
@@ -321,3 +321,5 @@ const myArray = [["John", 23], ["dog", 3]];
 myArray.shift();
 // Only change code below this line
 myArray.unshift(["Paul", 35]);
+
+const myList = [["Chocolate Bar", 15], ["Chocolate Bar", 15], ["Chocolate Bar", 15], ["Chocolate Bar", 15], ["hocolate Bar", 15]];
