405045c6187f5e3f055cd8bc2f9b9168bef52d44
Daniel <meskal1@rambler.ru>
2022-02-08 17:59:32 +0300

    задачa_37

--- script.js
+++ script.js
@@ -224,3 +224,7 @@ const myStr = "This is the start." + " This is the end."; // Change this line
 
 let myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.";
+
+// Only change code below this line
+const myName = "Daniel";
+const myStr = "My name is " + myName + " and I am well!";
