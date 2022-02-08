9718f28d474018e523ae3b45799bf6322cfb7dc5
Daniel <meskal1@rambler.ru>
2022-02-08 10:55:09 +0300

    задачa_15

--- script.js
+++ script.js
@@ -76,4 +76,13 @@ const HIGH_TEMPERATURES = {
 };
 // Only change code below this line 
 const { today, tomorrow } = HIGH_TEMPERATURES;
+// Only change code above this line
+
+const HIGH_TEMPERATURES = {
+	yesterday: 75,
+	today: 77,
+	tomorrow: 80
+};
+// Only change code below this line	
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
  // Only change code above this line
