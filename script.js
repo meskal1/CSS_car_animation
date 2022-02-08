77d7e52bcc776a41d65d83551682ef48d75f222b
Daniel <meskal1@rambler.ru>
2022-02-08 19:33:57 +0300

    задачa_42

--- script.js
+++ script.js
@@ -257,3 +257,8 @@ const lastName = "Lovelace";
 // Only change code below this line
 const thirdLetterOfLastName = lastName[2]; // Change this line
 
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1];; // Change this line
+
