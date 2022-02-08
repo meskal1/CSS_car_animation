72b5bdf14ea861a45cbc31b43711767e34d8688d
Daniel <meskal1@rambler.ru>
2022-02-08 20:19:59 +0300

    задачa_44

--- script.js
+++ script.js
@@ -267,3 +267,11 @@ const lastName = "Lovelace";
 // Only change code below this line
 const secondToLastLetterOfLastName = lastName[lastName.length - 2];; // Change this line
 
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+// Only change code below this line
+const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
+
