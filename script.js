f3b8286d777679f545adf1e4287aca9f6a9da6e9
Daniel <meskal1@rambler.ru>
2022-02-08 12:33:24 +0300

    задачa_18

--- script.js
+++ script.js
@@ -119,4 +119,20 @@ const stats = {
 };
 // Only change code below this line
 const half = ({ max, min }) => (max + min) / 2;
- // Only change code above this line
+// Only change code above this line
+
+const result = {
+	success: ["max-length", "no-amd", "prefer-arrow-functions"],
+	failure: ["no-var", "var-on-top", "linebreak"],
+	skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+	// Only change code below this line
+	const failureItems = [];
+	for (let i of arr)
+		failureItems.push(`<li class="text-warning">${i}</li>`);
+	// Only change code above this line
+	return failureItems;
+}
+const failuresList = makeList(result.failure);
+
