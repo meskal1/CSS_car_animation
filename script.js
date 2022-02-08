71119e98b012032304a16392313131d8316e0818
Daniel <meskal1@rambler.ru>
2022-02-08 21:23:06 +0300

    задачa_56

--- script.js
+++ script.js
@@ -315,3 +315,9 @@ const removedFromMyArray = myArray.pop();
 const myArray = [["John", 23], ["dog", 3]];
 // Only change code below this line
 const removedFromMyArray = myArray.shift();
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
